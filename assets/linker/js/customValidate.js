d54f0f03e5e1fb529e97566736c37503124de194
msalcala11 <msalcala11@gmail.com>
2013-12-20 00:54:16 -0500

    episode 6 additions

---
+++ assets/linker/js/customValidate.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+	//Validate
+	// http://bassistance.de/jquery-plugins/jquery-plugin-validation/
+	// http://docs.jquery.com/Plugins/Validation/
+	// http://docs.jquery.com/Plugins/Validationvalidate#toptions
+
+	//$( "#submitButton" ).click(function( event ) {
+ 	//	 event.preventDefault();
+	//}	
+
+		$(".form-signin").validate({
+			rules: {
+				name: {
+					required: true
+				},
+				email: {
+					required: true,
+					email: true
+				},
+				password: {
+					minlength: 6,
+					required: true
+				},
+				confirmation: {
+					minlength: 6,
+					equalTo: "#password"
+				}
+			},
+			success: function(element) {
+				element
+				.text('OK!').addClass('valid')
+			}
+		});
+
+
+
+});
